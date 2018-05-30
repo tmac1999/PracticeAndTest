b3cff9870a837e6d1d36ea2a77e12d3ed01f056c
zhengpeng <zhengpeng@gaosiedu.com>
2018-05-30 17:00:31 +0800

    test shadow line

---
+++ app/src/main/java/com/example/mrz/practiceandtest/ShadowLineActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mrz.practiceandtest
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ShadowLineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shadow_line)
+    }
+}
--- app/src/main/java/com/example/mrz/practiceandtest/TestImageViewActivity.java
+++ app/src/main/java/com/example/mrz/practiceandtest/TestImageViewActivity.java
@@ -2,45 +2,71 @@ package com.example.mrz.practiceandtest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+
+import com.example.mrz.practiceandtest.view.SlidingTabLayout;
+
+import java.util.ArrayList;
 
 public class TestImageViewActivity extends AppCompatActivity {
-    ListView lv = (ListView) findViewById(R.id.lv);
+//    ListView lv = (ListView) findViewById(R.id.lv);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_image_view);
-        ListView lv = (ListView) findViewById(R.id.lv);
-        lv.setAdapter(new BaseAdapter() {
+//        ListView lv = (ListView) findViewById(R.id.lv);
+//        lv.setAdapter(new BaseAdapter() {
+//            @Override
+//            public int getCount() {
+//                return 3;
+//            }
+//
+//            @Override
+//            public Object getItem(int position) {
+//                return null;
+//            }
+//
+//            @Override
+//            public long getItemId(int position) {
+//                return 0;
+//            }
+//
+//            @Override
+//            public View getView(int position, View convertView, ViewGroup parent) {
+//                View view = View.inflate(TestImageViewActivity.this, R.layout.item, null);
+////                ImageView img = view.findViewById(R.id.iv);
+////                img.setLayoutParams(new LinearLayout.LayoutParams(getScreenWidth(TestImageViewActivity.this),getScreenWidth(TestImageViewActivity.this)/2));
+//
+//                return view;
+//            }
+//        });
+        SlidingTabLayout id_slidingtab_indicator = (SlidingTabLayout) findViewById(R.id.id_slidingtab_indicator);
+        ViewPager vp = (ViewPager) findViewById(R.id.vp);
+        BlankFragment blankFragment = new BlankFragment();
+        BlankFragment blankFragment1 = new BlankFragment();
+        BlankFragment blankFragment2 = new BlankFragment();
+       final ArrayList<BlankFragment> list = new ArrayList<>();
+        list.add(blankFragment);
+        list.add(blankFragment1);
+       // list.add(blankFragment2);
+        vp.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public int getCount() {
-                return 3;
+                return list.size();
             }
 
             @Override
-            public Object getItem(int position) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                View view = View.inflate(TestImageViewActivity.this, R.layout.item, null);
-//                ImageView img = view.findViewById(R.id.iv);
-//                img.setLayoutParams(new LinearLayout.LayoutParams(getScreenWidth(TestImageViewActivity.this),getScreenWidth(TestImageViewActivity.this)/2));
-
-                return view;
+            public Fragment getItem(int position) {
+                return list.get(position);
             }
         });
+        String[] strings = {"阿萨德", "阿萨德到底"};
+        id_slidingtab_indicator.setViewPager(vp, strings);
+
     }
 
     public static int getScreenWidth(Activity activity) {
