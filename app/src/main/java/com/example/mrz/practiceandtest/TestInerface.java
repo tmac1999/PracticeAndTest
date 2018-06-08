1cddbe7e59ccff174b5adb8dfc63e26437acd2af
zhengpeng <zhengpeng@gaosiedu.com>
2018-06-08 17:05:01 +0800

    test constraint layout

---
+++ app/src/main/java/com/example/mrz/practiceandtest/TestInerface.java
@@ -0,0 +1,10 @@
+package com.example.mrz.practiceandtest;
+
+/**
+ * Created by mrz on 18/6/6.
+ */
+
+public interface TestInerface {
+    public String name = "asd";
+    BlankFragment qqq = null;
+}
