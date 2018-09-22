a5e21e9ff36210d31185bae6e047670df27418f0
zhengpeng <zhengpeng@gaosiedu.com>
2018-09-23 00:20:34 +0800

    test click area

---
+++ testjava/src/main/java/com/example/TestObjectMember.java
@@ -0,0 +1,37 @@
+package com.example;
+
+/**
+ * Created by mrz on 18/9/14.
+ */
+class Father {
+    public static String name;
+}
+
+class Child extends Father {
+
+    //    static {
+//        name = "sad";
+//    }
+    public static String name ;
+    public static String BASE = "base";
+    public static String URL = BASE+"asdad.com";
+
+    public void initName(String s) {
+        name = s;
+    }
+}
+
+public class TestObjectMember {
+    public static void main(String[] args) {
+        new Child().initName("sad");
+//        Print.pln(Father.name);
+//        Print.pln(Child.name);
+        Print.pln(Child.URL);
+        Child.BASE = "aaa";
+
+        Print.pln(Child.URL);
+
+    }
+}
+
+
