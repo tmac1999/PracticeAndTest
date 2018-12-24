7a03977cc4f43a21bf15c18d9d7d799966a8bf4a
zhengpeng <zhengpeng@gaosiedu.com>
2018-12-24 15:16:56 +0800

    test Style kit

---
+++ app/src/main/java/com/example/mrz/practiceandtest/paintcode/Shadow.java
@@ -0,0 +1,31 @@
+package com.gaosi.view.paintcode;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by mrz on 2018/12/6.
+ */
+
+public class Shadow extends View {
+    public Shadow(Context context) {
+        super(context);
+    }
+
+    public Shadow(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Shadow(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        StyleKitName.drawCanvas1(canvas);
+    }
+}
